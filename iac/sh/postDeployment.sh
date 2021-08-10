8525af3e60025505e1e337173269461d28a96fd8
Florian Wagner <florian.wagner@microsoft.com>
2021-08-10 02:49:24 +0000

    update post deployment script
    Signed-off-by: Florian Wagner <florian.wagner@microsoft.com>

--- iac/sh/postDeployment.sh
+++ iac/sh/postDeployment.sh
@@ -5,10 +5,16 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -a -G docker $1
-
+if [-n "$1" ]; then
+    sudo usermod -a -G docker $1
+fi
 
 export AGENT_ALLOW_RUNASROOT=1
 export RUNNER_ALLOW_RUNASROOT=1
-./githubActionsRunner.sh $2 $3
-./azurePipelinesAgent.sh $4 $5 $6
+if [-n "$2" ]; then
+    ./githubActionsRunner.sh $2 $3
+fi
+
+if [-n "$4" ]; then
+    ./azurePipelinesAgent.sh $4 $5 $6
+fi
